ed255035640cf064c986e7ff7d793e6feee6cddb
Nisha Boga <Nisha Boga@192.168.2.60>
2021-07-23 14:02:15 -0400

    New class is created

---
+++ gitrepo/printstatement.java
@@ -0,0 +1,12 @@
+package gitrepo;
+
+public class printstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Java project");
+
+	}
+
+}
